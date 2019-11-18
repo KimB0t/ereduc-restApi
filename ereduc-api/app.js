a170e4e1b4c5cd052d2bfc3909eeb5e36887fdf0
KimB0t <47450309+KimB0t@users.noreply.github.com>
2019-11-18 10:37:15 +0100

    db_example

--- ereduc-api/app.js
+++ ereduc-api/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config')
 
+//Middlewares
 app.use(bodyparser.json())
+app.use(cors());
 
 //import routes
 const postsRoute = require('./routes/posts');
-
 
 //Middlewares
 app.use('/posts', postsRoute);
@@ -16,5 +19,11 @@ app.use('/posts', postsRoute);
 app.get('/', (req, res) => {
     res.send("we are home");
 })
+
+//Connect to db
+mongoose.connect(process.env.DB_CONNECTION, 
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log("connected to db")
+    );
 
 app.listen(3000);
